3558303b163bc8b43545428103b8b3b2cf778565
Mayhul Arora <mayhul99@gmail.com>
2017-02-05 15:23:50 -0800

    fixed ua identification

--- js/display.js
+++ js/display.js
@@ -13,6 +13,7 @@ var timeWhenTurnEnded;
 
 $('document').ready(function(){
 
+	var ua = window.navigator.userAgent;
 	var iOS = !!ua.match(/iPad/i) || !!ua.match(/iPhone/i);
 	var webkit = !!ua.match(/WebKit/i);
 	var iOSSafari = iOS && webkit && !ua.match(/CriOS/i);
